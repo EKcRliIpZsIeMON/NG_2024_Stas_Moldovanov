e728ade74b97d4defe878550c742928548022c2b
Stas <snislavmoldova20@gmail.com>
2024-10-18 21:19:13 +0300

    Lesson 2 | task 2

---
+++ Lesson_2/Task_2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+// Osnovnu zadachu vukonav :3
+int main() {
+    int num0,num1,num2,num3,num4; 
+    cout << "Enter 1 value: ";
+    cin >> num0 ;
+    cout << "Enter 2 value: ";
+    cin >> num1 ;
+    cout << "Enter 3 value: ";
+    cin >> num2 ;
+    cout << "Enter 4 value: ";
+    cin >> num3 ;
+    cout << "Enter 5 value: ";
+    cin >> num4 ;
+    cout<< "*** *\n"
+        << "* * *\n"
+        << "* *\n" 
+        << "  *\n" 
+        << "  *\n" ;
+}
