e7a2966a2eaeb8c1200f402a3e81833fb7decbeb
Stas <snislavmoldova20@gmail.com>
2024-10-18 20:03:07 +0300

    Update

---
+++ Lesson_1/Task_6/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// shiiiiiit :>
+int main() {
+    while (true) {
+        cout << "        *\n       **\n      ***\n     ****\n    *****\n   ******\n  *******\n ********\n*********\n";
+        system("sleep 1");
+        system("clear");
+        cout << "*********\n********\n*******\n******\n*****\n****\n***\n**\n*\n";
+        system("sleep 1");
+        system("clear");
+    }
+}
